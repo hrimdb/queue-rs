77248bdb4d76e90f30813454cd172c137b6eabfd
zhenren <wangweizhen@bilibili.com>
2018-01-22 10:55:47 +0800

    style: fix some warning of clippy

--- src/OptimisticFIFOQueue/node.rs
+++ src/OptimisticFIFOQueue/node.rs
@@ -1,20 +1,20 @@
 use std::sync::atomic::AtomicPtr;
 
 #[derive(Debug)]
-pub struct Point_t<T> {
-    pub ptr: AtomicPtr<Node_t<T>>,
+pub struct Point<T> {
+    pub ptr: AtomicPtr<Node<T>>,
     pub tag: u64,
 }
 
 #[derive(Debug)]
-pub struct Queue_t<T> {
-    pub tail: AtomicPtr<Point_t<T>>,
-    pub head: AtomicPtr<Point_t<T>>,
+pub struct Queue<T> {
+    pub tail: AtomicPtr<Point<T>>,
+    pub head: AtomicPtr<Point<T>>,
 }
 
 #[derive(Debug)]
-pub struct Node_t<T> {
+pub struct Node<T> {
     pub data: Option<T>,
-    pub next: AtomicPtr<Point_t<T>>,
-    pub prev: AtomicPtr<Point_t<T>>,
+    pub next: AtomicPtr<Point<T>>,
+    pub prev: AtomicPtr<Point<T>>,
 }
--- src/OptimisticFIFOQueue/queue.rs
+++ src/OptimisticFIFOQueue/queue.rs
@@ -1,25 +1,25 @@
 use std::ptr;
-use OptimisticFIFOQueue::node::Queue_t;
-use OptimisticFIFOQueue::node::Point_t;
-use OptimisticFIFOQueue::node::Node_t;
+use OptimisticFIFOQueue::node::Queue;
+use OptimisticFIFOQueue::node::Point;
+use OptimisticFIFOQueue::node::Node;
 use std::sync::atomic::AtomicPtr;
 
-impl<T> Queue_t<T> {
-    pub fn new() -> Queue_t<T> {
-        let mut tail_n = Node_t {
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        let mut tail_n = Node {
             next: AtomicPtr::new(ptr::null_mut()),
             prev: AtomicPtr::new(ptr::null_mut()),
             data: None,
         };
-        return Queue_t {
-            tail: AtomicPtr::new(&mut Point_t {
+        Queue {
+            tail: AtomicPtr::new(&mut Point {
                 tag: 0,
                 ptr: AtomicPtr::new(&mut tail_n),
             }),
-            head: AtomicPtr::new(&mut Point_t {
+            head: AtomicPtr::new(&mut Point {
                 tag: 0,
                 ptr: AtomicPtr::new(&mut tail_n),
             }),
-        };
+        }
     }
 }

4f5f0b4e311cfad7eb5f4f7dce1631f51291cad4
真人 <weizhen.wang@duitang.com>
2018-01-22 23:30:24 +0800

    refactor: Default for node and queue

--- src/OptimisticFIFOQueue/node.rs
+++ src/OptimisticFIFOQueue/node.rs
@@ -1,4 +1,5 @@
 use std::sync::atomic::AtomicPtr;
+use std::ptr;
 
 #[derive(Debug)]
 pub struct Point<T> {
@@ -17,4 +18,30 @@ pub struct Node<T> {
     pub data: Option<T>,
     pub next: AtomicPtr<Point<T>>,
     pub prev: AtomicPtr<Point<T>>,
+}
+
+impl<T> Default for Queue<T> {
+    fn default() -> Queue<T> {
+        let mut tail_n = Node::default();
+        Queue {
+            tail: AtomicPtr::new(&mut Point {
+                tag: 0,
+                ptr: AtomicPtr::new(&mut tail_n),
+            }),
+            head: AtomicPtr::new(&mut Point {
+                tag: 0,
+                ptr: AtomicPtr::new(&mut tail_n),
+            }),
+        }
+    }
+}
+
+impl<T> Default for Node<T> {
+    fn default() -> Node<T> {
+        Node {
+            next: AtomicPtr::new(ptr::null_mut()),
+            prev: AtomicPtr::new(ptr::null_mut()),
+            data: None,
+        }
+    }
 }
--- src/OptimisticFIFOQueue/queue.rs
+++ src/OptimisticFIFOQueue/queue.rs
@@ -1,28 +1,7 @@
-use std::ptr;
 use OptimisticFIFOQueue::node::Queue;
 use OptimisticFIFOQueue::node::Point;
 use OptimisticFIFOQueue::node::Node;
 use std::sync::atomic::AtomicPtr;
-
-impl<T> Default for Queue<T> {
-    fn default() -> Queue<T> {
-        let mut tail_n = Node {
-            next: AtomicPtr::new(ptr::null_mut()),
-            prev: AtomicPtr::new(ptr::null_mut()),
-            data: None,
-        };
-        Queue {
-            tail: AtomicPtr::new(&mut Point {
-                tag: 0,
-                ptr: AtomicPtr::new(&mut tail_n),
-            }),
-            head: AtomicPtr::new(&mut Point {
-                tag: 0,
-                ptr: AtomicPtr::new(&mut tail_n),
-            }),
-        }
-    }
-}
 
 impl<T> Queue<T> {
     pub fn new() -> Queue<T> {
